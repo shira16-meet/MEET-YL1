7399de71e84900d6eea1a83e58df57490d01c82c
Shira Ivker <shira16@meet.mit.edu>
2014-11-30 18:37:39 +0200

    adding name 2

--- names.py
+++ names.py
@@ -1,6 +1,2 @@
-Python 2.7.3 (default, Feb 27 2014, 20:00:17) 
-[GCC 4.6.3] on linux2
-Type "copyright", "credits" or "license()" for more information.
-==== No Subprocess ====
->>> 
-print "quel"
+print ("quel")
+print ("dylan")
